431f3b2733f89f341f334e600acd22fba8a5829a
zobirofkir <zobirofkir19@gmail.com>
2024-11-27 22:04:32 +0100

    create categories

---
+++ app/Models/Category.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "user_id",
+        "title",
+        "description",
+        "image",
+        "slug"
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($blog) {
+            $blog->slug = Str::slug($blog->title);
+        });
+
+        static::updating(function ($blog) {
+            if ($blog->isDirty('title')) {
+                $blog->slug = Str::slug($blog->title);
+            }
+        });
+    }
+
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
