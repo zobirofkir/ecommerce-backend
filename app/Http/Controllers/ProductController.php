03c13fe3ba151c0a5b8ac001886be72a380bef82
zobirofkir <zobirofkir19@gmail.com>
2024-12-01 14:54:23 +0100

    add return type

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,26 +5,28 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Services\Facades\ProductFacade;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use App\Http\Resources\ProductResource;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index() : AnonymousResourceCollection
     {
         return ProductFacade::index();
     }
 
 
-    public function show(Product $product, $slug)
+    public function show(Product $product, $slug) : ProductResource
     {
         return ProductFacade::show($product, $slug);
     }
 
-    public function categoryProducts($categorySlug)
+    public function categoryProducts($categorySlug) : AnonymousResourceCollection
     {
         return ProductFacade::categoryProducts($categorySlug);
     }
 
-    public function search(Request $request)
+    public function search(Request $request) : AnonymousResourceCollection
     {
         $query = $request->input('query');
         return ProductFacade::search($query);
