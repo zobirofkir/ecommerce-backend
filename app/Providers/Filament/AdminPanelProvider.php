a195abb693cf0e9a3f0fb26cf8400fb150af2477
zobirofkir <zobirofkir19@gmail.com>
2024-12-02 19:44:51 +0100

    update path url

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('/users')
+            ->path('/')
             ->login()
             ->colors([
                 'primary' => [
